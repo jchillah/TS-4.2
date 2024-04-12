5b2990c344a058351c5563b52cfa69568877a597
jchillah <jchillah@gmail.com>
2024-04-12 14:09:55 +0200

    Update animal.dart

--- domain/animal.dart
+++ domain/animal.dart
@@ -10,11 +10,4 @@ class Animal {
     required this.imagePath,
     this.soundPath, required String sound, required String image, required videoPath, required isShowingVideo,
   });
-/*
-  // Beispielimplementation für soundPath verschoben in
-  VideoContent? getSoundPath() {
- 
-    return soundPath;
-  }      
-  */
 }
